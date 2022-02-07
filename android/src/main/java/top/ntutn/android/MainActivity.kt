83d0dfee34ba3b5e6e7cedd60984380b2ed205d5
zerofancy <ntutn.top@gmail.com>
2022-02-07 20:52:14 +0800

    First Commit

---
+++ android/src/main/java/top/ntutn/android/MainActivity.kt
@@ -0,0 +1,18 @@
+package top.ntutn.android
+
+import top.ntutn.common.App
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                App()
+            }
+        }
+    }
+}
