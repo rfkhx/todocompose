28c87d14fa9a10a4ec3e88f39ed12efc950407b7
zerofancy <ntutn.top@gmail.com>
2022-02-28 12:21:30 +0800

    Android竖屏显示

--- common/src/commonMain/kotlin/top/ntutn/common/ui/PortraitRootContent.kt
+++ common/src/commonMain/kotlin/top/ntutn/common/ui/PortraitRootContent.kt
@@ -1,6 +1,11 @@
 package top.ntutn.common.ui
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.jetbrains.Children
 import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.crossfadeScale
@@ -11,10 +16,24 @@ import top.ntutn.common.component.PortraitRootComponent
 @OptIn(ExperimentalDecomposeApi::class)
 @Composable
 fun PortraitRootContent(rootComponent: PortraitRootComponent) {
-    Children(routerState = rootComponent.routerState, animation = crossfadeScale()) {
-        when (val child = it.instance) {
-            is PortraitRoot.Child.Counter -> CounterUi(child.component)
-            is PortraitRoot.Child.About -> AboutUI(child.component)
+    Column {
+        Box(modifier = Modifier.fillMaxWidth().weight(1f)) {
+            Children(routerState = rootComponent.routerState, animation = crossfadeScale()) {
+                when (val child = it.instance) {
+                    is PortraitRoot.Child.Counter -> CounterUi(child.component)
+                    is PortraitRoot.Child.About -> AboutUI(child.component)
+                    is PortraitRoot.Child.Todo -> TodoUI(child.component)
+                    else -> throw NotImplementedError("未绑定UI")
+                }
+            }
+        }
+        Row(modifier = Modifier.padding(16.dp)) {
+            Button(onClick = rootComponent::onTodoPage) {
+                Text("TODO")
+            }
+            Button(onClick = rootComponent::onAboutPage) {
+                Text("关于")
+            }
         }
     }
 }
