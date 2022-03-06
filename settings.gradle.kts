84d834570488b11dfad477c8b32c75b2a0ad8b2c
zerofancy <ntutn.top@gmail.com>
2022-03-07 00:33:32 +0800

    基于buildSrc的简单依赖管理

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,29 +6,6 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://jitpack.io")
     }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.toString() == "com.arkivanov.gradle.setup") {
-                useModule("com.github.arkivanov:gradle-setup-plugin:9f046bd98f")
-            }
-            if (requested.id.id == "com.squareup.sqldelight") {
-                useModule("com.squareup.sqldelight:gradle-plugin:1.5.3")
-            }
-        }
-    }
-    plugins {
-        // TODO 使用extra管理這些配置
-        val kotlinVersion = "1.6.10"
-        val agpVersion = "7.0.4"
-        val composeVersion = "1.0.1"
-
-        kotlin("jvm").version(kotlinVersion)
-        kotlin("multiplatform").version(kotlinVersion)
-        kotlin("android").version(kotlinVersion)
-        id("com.android.application").version(agpVersion)
-        id("com.android.library").version(agpVersion)
-        id("org.jetbrains.compose").version(composeVersion)
-    }
 }
 rootProject.name = "todocompose"
 
